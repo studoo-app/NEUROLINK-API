3d8e21ff65a8a9b28d541dc87d1e6d49cfd517a7
nours <j.pechberty@gmail.com>
2026-09-14 14:22:47 +0200

    Add scripts and app content

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+from app.startup import run_startup
+from core.database import Base, engine
+from features.auth.router import router as auth_router
+from features.hello.router import router as hello_router
+from features.implants.router import router as implants_router
+
+app = FastAPI(
+    title="NeuroLink API",
+    description="API for managing implants and related data",
+    version="1.0.0",
+)
+
+api_route_prefix = "/api/v1"
+
+Base.metadata.create_all(bind=engine)
+run_startup()
+
+app.include_router(hello_router, prefix=api_route_prefix)
+app.include_router(auth_router, prefix=api_route_prefix)
+app.include_router(implants_router, prefix=api_route_prefix)
